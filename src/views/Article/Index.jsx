9702b0967ae3aef64d29e31ff784e263ac039070
leksyib <leksyib14@gmail.com>
2019-04-17 18:20:32 +0100

    ft(social share): add social sharing buttons
    - add react-share package for social sharing buttons for whatsapp, facebook, google, twitter and linkedin
    
    [Finishes #164139655]

--- src/views/Article/Index.jsx
+++ src/views/Article/Index.jsx
@@ -2,25 +2,47 @@
 import React, { useEffect } from 'react';
 import {
   Container,
-  CardDeck
+  CardDeck,
+  Row,
+  Col
 } from 'react-bootstrap';
 import PropTypes from 'prop-types';
+import {
+  FacebookShareCount,
+  LinkedinShareCount,
+  FacebookShareButton,
+  LinkedinShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+  FacebookIcon,
+  TwitterIcon,
+  LinkedinIcon,
+  EmailIcon,
+  WhatsappIcon,
+  EmailShareButton
+} from 'react-share';
 import { connect } from 'react-redux';
 import ArticleTitle from '../../components/articleTitle/Index';
 import ArticleBody from '../../components/articleBody/Index';
 import ImageContainer from '../../components/imageContainer/Index';
 import ArticleSummary from '../../components/articleSummary/Index';
 import ArticleDescription from '../../components/articleDescription/Index';
-import { getArticleRequest, getRecommendedArticles } from '../../redux/actions/articles';
+import {
+  getArticleRequest,
+  getRecommendedArticles
+} from '../../redux/actions/articles';
 import Footer from '../../components/footer/Index';
 import Loader from '../../components/loader/Index';
-import Button from '../../components/button/Index';
 import './article.scss';
-
 
 const Article = (props) => {
   const { article, getArticle, getRecommendedArticles: getArticles } = props;
-  const { match: { params: { slug } } } = props;
+  const {
+    match: {
+      params: { slug }
+    }
+  } = props;
+  const shareUrl = `https://vidar-ah-frontend-staging.herokuapp.com/articles/${slug}`;
   useEffect(() => {
     getArticle(slug);
     getArticles();
@@ -33,28 +55,77 @@ const Article = (props) => {
       <Container>
         <ArticleTitle title={article.title} />
         <ArticleDescription description={article.description} />
-        <ImageContainer src={article.images[0] || 'https://res.cloudinary.com/djdsxql5q/image/upload/v1554806589/Authors%20Haven/culture.jpg'} />
+        <ImageContainer
+          src={
+            article.images[0]
+            || 'https://res.cloudinary.com/djdsxql5q/image/upload/v1554806589/Authors%20Haven/culture.jpg'
+          }
+        />
         <ArticleBody body={article.body} />
-        <div className="edit-delete-container">
-          <Button className="btn" text="Edit" />
-          <Button className="btn" text="Delete" />
-        </div>
+      </Container>
+      <Container>
+        <Row>
+          <Col>
+            <WhatsappShareButton
+              url={shareUrl}
+              title={article.title}
+              separator=":"
+            >
+              <WhatsappIcon size={32} round />
+            </WhatsappShareButton>
+          </Col>
+          <Col>
+            <LinkedinShareButton
+              url={shareUrl}
+              title={article.title}
+              windowWidth={750}
+              windowHeight={600}
+            >
+              <LinkedinIcon size={32} round />
+            </LinkedinShareButton>
+
+            <LinkedinShareCount url={shareUrl}>
+              {count => count}
+            </LinkedinShareCount>
+          </Col>
+          <Col>
+            <FacebookShareButton url={shareUrl} quote={article.title}>
+              <FacebookIcon size={32} round />
+            </FacebookShareButton>
+
+            <FacebookShareCount url={shareUrl}>
+              {count => count}
+            </FacebookShareCount>
+          </Col>
+          <Col>
+            <TwitterShareButton url={shareUrl} title={article.title}>
+              <TwitterIcon size={32} round />
+            </TwitterShareButton>
+          </Col>
+          <Col>
+            <EmailShareButton
+              url={shareUrl}
+              subject={article.title}
+              body="body"
+            >
+              <EmailIcon size={32} round />
+            </EmailShareButton>
+          </Col>
+        </Row>
       </Container>
       <hr />
       <Container>
         <h3>Also recommended for you</h3>
         <CardDeck>
-          {
-            props.recommendedArticles.map(recArticle => (
-              <ArticleSummary
-                key={recArticle.id}
-                src={recArticle.images[0]}
-                header={recArticle.title}
-                url={`/articles/${recArticle.slug}`}
-                time={recArticle.updatedAt}
-              />
-            ))
-          }
+          {props.recommendedArticles.map(recArticle => (
+            <ArticleSummary
+              key={recArticle.id}
+              src={recArticle.images[0]}
+              header={recArticle.title}
+              url={`/articles/${recArticle.slug}`}
+              time={recArticle.updatedAt}
+            />
+          ))}
         </CardDeck>
       </Container>
       <Footer />
@@ -83,5 +154,9 @@ const mapDispatchToProps = dispatch => ({
   getRecommendedArticles: () => dispatch(getRecommendedArticles())
 });
 
-const ConnectedArticle = connect(mapStateToProps, mapDispatchToProps)(Article);
+const ConnectedArticle = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Article);
+
 export default ConnectedArticle;
